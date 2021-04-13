01d3a997569b116df9057925d0473c93e7f155f6
Rishikeshk9 <rushikeshkardile9@gmail.com>
2021-04-13 05:47:36 +0530

    Fixed Mobile Responsiveness & Added About Us Page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    // Transition effect for navbar 
+
+     // Transition effect for navbar 
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
       if($(this).scrollTop() > 500) { 
@@ -8,24 +9,25 @@ $(document).ready(function() {
           $('.navbar').removeClass('solid');
       }
     });
-
+    
     $('.home').addClass('active');
    
-    barba.init({
-        transitions: [{
-          name: 'opacity-transition',
-          leave(data) {
-            return gsap.to(data.current.container, {
-              opacity: 0
-            });
-          },
-          enter(data) {
-            return gsap.from(data.next.container, {
-              opacity: 0
-            });
-          }
-        }]
-      });
+    var videos = document.getElementsByTagName("video"), fraction = 0.8;
+
+   
+    for(var i = 0; i < videos.length; i++) {
+  
+        var video = videos[i];
+
+        video.volume = 0.25;
+       if (video.canplay)
+        {
+          video.play();
+           
+        }
+    }
+
+     
 });
 
 
@@ -69,11 +71,44 @@ function myfunction()
     header.classList.toggle('menu-opened');
  }
   
-        new kursor({
-            type: 2,
-            removeDefaultCursor: true,
-            color: '#0399e0'
+new kursor({
+    type: 2,
+    removeDefaultCursor: true,
+    color: '#0399e0'
 
 
-        })
+})
+ 
+  var videos = document.getElementsByTagName("video"), fraction = 0.8;
+
+  function checkScroll() {
+
+  for(var i = 0; i < videos.length; i++) {
+
+      var video = videos[i];
+
+      var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+          b = y + h, //bottom
+          visibleX, visibleY, visible;
+
+          visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+          visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+          visible = visibleX * visibleY / (w * h);
+
+          if (visible > fraction) {
+              video.play(); 
+              video.controls = true; 
+
+          } else { 
+            video.pause(); 
+          }
+
+  }
+
+  }
+
+  window.addEventListener('scroll', checkScroll, false);
+  window.addEventListener('resize', checkScroll, false);
+  
     
